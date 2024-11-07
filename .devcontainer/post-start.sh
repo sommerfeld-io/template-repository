7829d6ee67acf898aaf25e05c2756494cd58d15f
Sebastian <101367275+sebastian-sommerfeld-io@users.noreply.github.com>
2024-11-07 09:21:49 +0100

    refactor: bash settings

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -3,5 +3,10 @@
 ##
 ## @see .devcontainer/devcontainer.json
 
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
 echo "[INFO] Starting portainer"
 docker compose --file .devcontainer/ops/docker-compose.yml --env-file .devcontainer/ops/.env up -d
