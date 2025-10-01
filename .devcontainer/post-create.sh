31c3b50e14c46945ca261b1e8ed07783b5bee375
sebastian <sebastian@sommerfeld.io>
2025-10-01 12:19:04 +0000

    chore: remove portainer from devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-## Install tools and dependencies for the project.
-## This script is run after the devcontainer is created.
-##
-## @see .devcontainer/devcontainer.json
 
 set -o errexit
 set -o pipefail
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-## Start Service after the devcontainer is started.
-##
-## @see .devcontainer/devcontainer.json
 
 set -o errexit
 set -o pipefail
 set -o nounset
 # set -o xtrace
-
-echo "[INFO] Starting portainer"
-docker compose --file .devcontainer/ops/docker-compose.yml --env-file .devcontainer/ops/.env up -d
