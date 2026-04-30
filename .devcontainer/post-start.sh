24944ffa304b27702ae88f08a32d32f02ce4a6ba
Sebastian Sommerfeld <101367275+sebastian-sommerfeld-io@users.noreply.github.com>
2026-04-30 15:41:30 +0200

    fix(devcontainer): update Dockerfile and post-start script for `nftables` support to allow using docker inside devcontainer on ubuntu 26.04

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,3 +4,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 # set -o xtrace
+
+## Use nftables backend for iptables to ensure Docker networking works on Ubuntu 26.04+
+## TODO ... check if this is needed after a docker or devcontainer or dind feature update
+sudo update-alternatives --set iptables /usr/sbin/iptables-nft
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
