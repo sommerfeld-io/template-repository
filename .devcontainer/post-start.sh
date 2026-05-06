34d1aa7aa747d2270ec91b47f9d56229ee890f07
Sebastian Sommerfeld <101367275+sebastian-sommerfeld-io@users.noreply.github.com>
2026-05-06 09:13:49 +0200

    docs(devcontainer): update TODO comment with issue link for nftables backend verification

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,6 +6,6 @@ set -o nounset
 # set -o xtrace
 
 ## Use nftables backend for iptables to ensure Docker networking works on Ubuntu 26.04+
-## TODO ... check if this is needed after a docker or devcontainer or dind feature update
+## <https://github.com/orgs/sommerfeld-io/projects/1/views/1?pane=issue&itemId=184218287>
 sudo update-alternatives --set iptables /usr/sbin/iptables-nft
 sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
